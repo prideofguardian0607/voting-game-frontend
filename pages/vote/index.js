0502de756b1acd6477708066c23b16a35e1da675
SageDragonKing <sagedragon915@gmail.com>
2022-04-08 21:26:10 +0300

    Update index.js

--- pages/vote/index.js
+++ pages/vote/index.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 import Button from '@mui/material/Button';
 import { useEffect } from 'react'
-import io from 'Socket.IO-client'
+import io from 'socket.io-client'
 let socket
 
 export default function Vote() {
